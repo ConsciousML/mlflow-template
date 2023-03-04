7e4433b01e086e75b0d580fdf73cfd8cd337f017
Axel Mendoza <axel.mendoza@epita.fr>
2023-03-04 18:15:34 +0100

    add logging for mnist training

--- mymodule/mnist_pytorch_training.py
+++ mymodule/mnist_pytorch_training.py
@@ -1,6 +1,7 @@
 """Train a simple neural network on the MNIST dataset using PyTorch.
 For more information, see https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html"""
 import torch
+import mlflow
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim import Optimizer
@@ -72,11 +73,12 @@ def train(
         optimizer.step()
 
         loss = loss.item()
+        mlflow.log_metric('train_loss', loss, step=batch)
         print(f"loss: {loss:>7f}  [{batch:>5d}/{nb_batches:>5d}]")
 
 
 def test(
-    dataloader: DataLoader, model: nn.Module, loss_fn: nn.Module, device: str = 'cuda'
+    epoch: int, dataloader: DataLoader, model: nn.Module, loss_fn: nn.Module, device: str = 'cuda'
 ) -> None:
     """This function tests a neural network on the mnist test dataset."""
     size = float(len(dataloader.dataset))  # type: ignore
@@ -90,8 +92,12 @@ def test(
             test_loss += loss_fn(pred, ground_truth).item()
             correct += (pred.argmax(1) == ground_truth).type(torch.float).sum().item()
     test_loss /= num_batches
+
     correct /= size
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    mlflow.log_metric('test_loss', test_loss, step=epoch)
+    mlflow.log_metric('accuracy', correct, step=epoch)
 
 
 def mnist_pytorch_training(
@@ -104,6 +110,12 @@ def mnist_pytorch_training(
         batch_size (int, optional): This is the batch size to use for training. Defaults to 64.
         train_on_first_n (int, optional): This is the number of samples to train on. Defaults to 0.
     """
+    mlflow.set_experiment('mnist_pytorch_training')
+    mlflow.log_param('epochs', epochs)
+    mlflow.log_param('batch_size', batch_size)
+    mlflow.log_param('train_on_first_n', train_on_first_n)
+    mlflow.log_param('dataset', 'FashionMNIST')
+
     training_data = datasets.FashionMNIST(
         root="data",
         train=True,
@@ -141,12 +153,12 @@ def mnist_pytorch_training(
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}\n-------------------------------")
         train(train_dataloader, model, loss_fn, optimizer, device=device)
-        test(test_dataloader, model, loss_fn, device=device)
+        test(epoch, test_dataloader, model, loss_fn, device=device)
     print("Done!")
 
 
 if __name__ == '__main__':
-    mnist_pytorch_training(epochs=1, batch_size=64, train_on_first_n=1000)
+    mnist_pytorch_training(epochs=5, batch_size=64)
