7bd4dedeb6ba09fe5f7c13314f0e1a8ebff163b4
Axel Mendoza <axel.mendoza@epita.fr>
2023-03-13 19:04:18 +0100

    use with mlflow.startrun()

--- mymodule/mnist_pytorch_training.py
+++ mymodule/mnist_pytorch_training.py
@@ -2,6 +2,7 @@
 For more information, see https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html"""
 import torch
 import mlflow
+import mlflow.pytorch
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim import Optimizer
@@ -94,7 +95,7 @@ def test(
     test_loss /= num_batches
 
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \nAccuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
     mlflow.log_metric('test_loss', test_loss, step=epoch)
     mlflow.log_metric('accuracy', correct, step=epoch)
@@ -110,12 +111,6 @@ def mnist_pytorch_training(
         batch_size (int, optional): This is the batch size to use for training. Defaults to 64.
         train_on_first_n (int, optional): This is the number of samples to train on. Defaults to 0.
     """
-    mlflow.set_experiment('mnist_pytorch_training')
-    mlflow.log_param('epochs', epochs)
-    mlflow.log_param('batch_size', batch_size)
-    mlflow.log_param('train_on_first_n', train_on_first_n)
-    mlflow.log_param('dataset', 'FashionMNIST')
-
     training_data = datasets.FashionMNIST(
         root="data",
         train=True,
@@ -148,17 +143,23 @@ def mnist_pytorch_training(
     print(f"Using {device} device")
 
     model = NeuralNetwork().to(device)
-    print(model)
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    for epoch in range(epochs):
-        print(f"Epoch {epoch+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer, device=device)
-        test(epoch, test_dataloader, model, loss_fn, device=device)
-    print("Done!")
+    with mlflow.start_run():
+        mlflow.set_experiment('mnist_pytorch_training')
+        mlflow.log_param('epochs', epochs)
+        mlflow.log_param('batch_size', batch_size)
+        mlflow.log_param('train_on_first_n', train_on_first_n)
+        mlflow.log_param('dataset', 'FashionMNIST')
+        mlflow.pytorch.log_model(model, "model")
+
+        for epoch in range(epochs):
+            print(f"Epoch {epoch+1}\n-------------------------------")
+            train(train_dataloader, model, loss_fn, optimizer, device=device)
+            test(epoch, test_dataloader, model, loss_fn, device=device)
 
 
 if __name__ == '__main__':
-    mnist_pytorch_training(epochs=5, batch_size=64)
+    mnist_pytorch_training(epochs=1, batch_size=64)
