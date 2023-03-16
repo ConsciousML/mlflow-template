0d225ca1f99ba0c55e6ca5a68efbe6e6ad645780
Axel Mendoza <axel.mendoza@epita.fr>
2023-03-16 16:20:47 +0100

    add argparse to train + args to MLflow project

--- train.py
+++ train.py
@@ -1,5 +1,19 @@
 """Train a model on MNIST data."""
+from argparse import ArgumentParser
+
 from mymodule.mnist_pytorch_training import mnist_pytorch_training
 
 if __name__ == '__main__':
-    mnist_pytorch_training(epochs=1, batch_size=64, train_on_first_n=126)
+    parser = ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs to train the model')
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size to use for training')
+    parser.add_argument(
+        '--train_on_first_n',
+        type=int,
+        default=0,
+        help='Number of samples to train on. If 0 train on all samples',
+    )
+    args = parser.parse_args()
+    mnist_pytorch_training(
+        epochs=args.epochs, batch_size=args.batch_size, train_on_first_n=args.train_on_first_n
+    )
